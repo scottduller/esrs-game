1e64acb366f4099ba51992f30c443561348f9676
JFenton1997 <jfenton1997@gmail.com>
2021-05-11 01:36:40 +0100

    Added Level Save Menu
    + included icon assets

---
+++ ESRSGame/Assets/Scripts/GridBuildSystem/UI/LevelSaveUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GridBuildSystem.UI
+{
+    public class LevelSaveUI : MonoBehaviour
+    {
+        private GameObject _saveOptionsPane;
+
+        private void Start()
+        {
+            _saveOptionsPane = transform.Find("LevelSavePanel").gameObject;
+        
+            _saveOptionsPane.transform.Find("CloseBtn").GetComponent<Button>().onClick.AddListener(CloseMenu);
+        
+            _saveOptionsPane.transform.Find("SubmitBtn").GetComponent<Button>().onClick.AddListener(Submit);
+        
+        }
+
+        private void Submit()
+        {
+            string title = _saveOptionsPane.transform.Find("TitleInput").GetComponent<TMP_InputField>().text;
+            string desc = _saveOptionsPane.transform.Find("DescInput").GetComponent<TMP_InputField>().text;
+            if (title != string.Empty && desc != String.Empty)
+            {
+                LevelBuilderManager.Instance.SaveLevel(title,"test",desc);
+                CloseMenu();
+            }
+            else
+            {
+                Debug.Log("INPUT ERROR");
+            }
+        }
+
+
+        private void CloseMenu()
+        {
+            _saveOptionsPane.SetActive(false);
+        
+        }
+    
+        public void OpenMenu()
+        {
+            _saveOptionsPane.SetActive(true);
+        
+        }
+
+    }
+}
